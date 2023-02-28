3e60c8fb309235cf0b8e1b43ddc72677eeb82086
algoritmika14 <algomklass@gmail.com>
2023-02-28 23:11:25 +0500

    Initial commit

---
+++ Space Shooter/Assets/Code/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class Player : MonoBehaviour
+{
+    public int points;
+
+    void Move()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 mousePos = Input.mousePosition;
+            Vector2 realPos = Camera.main.ScreenToWorldPoint(mousePos);
+            transform.position = realPos;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+}
